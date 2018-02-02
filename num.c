4b9e38bcfacacadeca13076dc8b45033657c070e
karthikeyan2 <35687514+karthikeyan2@users.noreply.github.com>
2018-02-02 16:02:25 +0530

    num.c

---
+++ num.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter a num: ");
+    scanf("%lf", &num);
+
+    if (num <= 0.0)
+    {
+        if (number == 0.0)
+            printf("You entered 0.");
+        else
+            printf("You entered a negative num.");
+    }
+    else
+        printf("You entered a positive num.");
+    return 0;
+}
